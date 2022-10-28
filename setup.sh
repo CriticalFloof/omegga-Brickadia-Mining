2300d4004ba584354cf09f74681f124523cf2fd8
CriticalFloof <104379154+CriticalFloof@users.noreply.github.com>
2022-10-28 11:21:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-mkdir -p ../../data/Saved/Builds/BrickadiaMining-Structures && cp -r src/Data/BrickadiaMining-Structures ../../data/Saved/Builds/BrickadiaMining-Structures
+mkdir -p ../../data/Saved/Builds && cp -r src/Data/BrickadiaMining-Structures ../../data/Saved/Builds/BrickadiaMining-Structures
 mkdir -p ../../data/Saved/Presets/Environment && cp src/Data/BrickadiaMining-Environment/Brickadia-Mining-Plate* ../../data/Saved/Presets/Environment/Brickadia-Mining-Plate.bp
